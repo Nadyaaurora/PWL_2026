3cad3c1680e987e881e984a8b2bed8d5efe22af6
nadyaaurora <nadyaaurora575@gmail.com>
2026-02-23 14:07:21 +0700

    PRAKTIKUM 3

--- app/Http/Controllers/WelcomeController.php
+++ app/Http/Controllers/WelcomeController.php
@@ -7,4 +7,12 @@ class WelcomeController extends Controller
     public function hello() {
         return 'Hello World';
     }
-}
+
+    public function greeting(){
+        return view('blog.hello')
+            ->with('name','Nadya')
+            ->with('occupation','Data Analyst');
+        }
+    }
+
+
